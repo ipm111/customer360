9870fac5111c46991a3c02eac89156063f9721e2
ipm111 <ipm000@gmail.com>
2025-08-05 18:48:50 -0600

    Leccion 6 namespac2e

--- clientes/urls.py
+++ clientes/urls.py
@@ -6,7 +6,6 @@ app_name = 'clientes'  # << ESTE es el namespace
 
 
 urlpatterns = [
-    path('lista', views.lista, name='lista'),
- 
+    path('lista', views.lista, name='lista'), 
     path('nuevo/', views.nuevo_cliente, name='nuevo_cliente'),
 ]
