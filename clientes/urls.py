ad8c6e70e78f74da212e2e34f3ccb5e951a9b8c6
ipm111 <ipm000@gmail.com>
2025-08-06 20:12:25 -0600

    leccion8

--- clientes/urls.py
+++ clientes/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-
 from . import views 
 
 app_name = 'clientes'  # << ESTE es el namespace
 
 
+
 urlpatterns = [
     path('lista', views.lista, name='lista'), 
-    path('nuevo/', views.nuevo_cliente, name='nuevo_cliente'),
+    path('nuevo/', views.nuevo_cliente, name='nuevo'),
+    path('editar/<int:cliente_id>/', views.editar_cliente, name='editar'),
+    path('eliminar/<int:cliente_id>/', views.eliminar_cliente, name='eliminar'),
+
 ]
--- clientes/views.py
+++ clientes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from .models import Cliente
 from .forms import ClienteForm
 
@@ -6,7 +6,9 @@ from .forms import ClienteForm
 # Create your views here.
 def lista(request):
     clientes = Cliente.objects.all()
+    
     return render(request, 'clientes/lista.html', {'clientes': clientes})
+    
 
 
 
@@ -21,3 +23,22 @@ def nuevo_cliente(request):
 
     return render(request, 'clientes/nuevo_cliente.html', {'form': form})
 
+
+
+def editar_cliente(request, cliente_id):
+    cliente = get_object_or_404(Cliente, id=cliente_id)
+    if request.method == 'POST':
+        form = ClienteForm(request.POST, instance=cliente)
+        if form.is_valid():
+            form.save()
+            return redirect('clientes:lista')
+    else:
+        form = ClienteForm(instance=cliente)
+    return render(request, 'clientes/editar.html', {'form': form})
+
+def eliminar_cliente(request, cliente_id):
+    cliente = get_object_or_404(Cliente, id=cliente_id)
+    if request.method == 'POST':
+        cliente.delete()
+        return redirect('clientes:lista')
+    return render(request, 'clientes/eliminar.html', {'cliente': cliente})
